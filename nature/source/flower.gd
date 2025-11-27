296592393647e3675747639964f48f4009875718
JasonOS03 <=>
2025-11-27 18:12:40 +0000

    added the functionality to display the count of pollinated flowers on the screen. The label node is used to hold the text value. the total number of pollinations that have occurred are displayed to the user. The _all_flowers_pollinated() function as been moved to the garden script to allow for centralized logic, The garden itself should keep a running total of the number pollinated rather than each individual flower holding the total.

--- nature/source/flower.gd
+++ nature/source/flower.gd
@@ -13,14 +13,6 @@ func _bee_click():
 		particles.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
 	pollinated_count += 1
 	print("Number pollinated: ", pollinated_count)
-	_all_flowers_pollinated()
+	get_parent()._all_flowers_pollinated()
 	return pollinated_count
 	
-func _all_flowers_pollinated():
-	var total_flowers_pollinated = 0
-	for crocus_flower in get_tree().get_nodes_in_group("flowers"):
-		total_flowers_pollinated += crocus_flower.pollinated_count
-	if total_flowers_pollinated == get_tree().get_nodes_in_group("flowers").size():
-		print("you have pollinated all of the flowers")
-	else:
-		print("remaining flowers left to pollinate: ",get_tree().get_nodes_in_group("flowers").size() - total_flowers_pollinated)
--- nature/source/garden.gd
+++ nature/source/garden.gd
@@ -3,15 +3,30 @@ extends Node3D
 var flower := preload("res://nature/source/flower.tscn");
 var flower_rows := 3.0;
 var flower_cols := 3.0;
+var label: Label 
+@export var total_flowers_pollinated: int = 0
 
 func _ready() -> void:
+	label = get_node("pollinated_count_label")
+	label.text = "number pollinated: " + str(total_flowers_pollinated)
 	for fl in range (flower_rows):
 		for cols in range (flower_cols):
 			var crocus_flower = flower.instantiate()
+			crocus_flower.add_to_group("flower")
 			crocus_flower.position = Vector3(fl*flower_rows,-0.5,cols*flower_cols)
 			add_child(crocus_flower)
 			print("flower location: ",crocus_flower.position)
 		
 		
+func _all_flowers_pollinated():
+	var total_flowers_pollinated = 0
+	for crocus_flower in get_tree().get_nodes_in_group("flower"):
+		total_flowers_pollinated += crocus_flower.pollinated_count
 		
+	label.text = "Flowers pollinated: " + str(total_flowers_pollinated)
+		
+	if total_flowers_pollinated >= get_tree().get_nodes_in_group("flower").size():
+		print("you have pollinated all of the flowers")
+	else:
+		print("remaining flowers left to pollinate: ",get_tree().get_nodes_in_group("flower").size() - total_flowers_pollinated)
 		
