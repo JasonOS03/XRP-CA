0e44aba0bce82005dc0ec2b33fd2d667cdc70b1e
JasonOS03 <=>
2025-11-28 01:58:38 +0000

    added the functionality for the unbloomed flower to transform into a bloomed flower upon pollination. The flower transforms from an unbloomed crocus flower into a bloomed rose flower, meant to look like a bloomed crocus flower. The rose will be colored with the same colors as the crocus flower. The bloomed flower was added to the scene tree as a new node, and the visible is toggled depending on whether the crocus flower has been pollinated or not. the bloomedflower transform is set to be the same as the unbloomed flower transform, to ensure the bloomed flower is in the same position, has the same scale and has the same rotation as the unbloomed flower

--- nature/source/flower.gd
+++ nature/source/flower.gd
@@ -1,14 +1,22 @@
 extends Area3D
 
 var pollinated_count = 0
-var particles: GPUParticles3D
+var particles: GPUParticles3D 
+var unbloomed_mesh = MeshInstance3D
+var bloomed_mesh = Node3D
 
 func _ready() -> void:
 	particles = get_node("GPUParticles3D")
-
+	unbloomed_mesh = get_node("12974CrocusFlowerV1L4")
+	bloomed_mesh = get_node("BloomedMesh2")
+	bloomed_mesh.transform = unbloomed_mesh.transform
+	bloomed_mesh.visible = false
+	
 func _bee_click():
 	print("Pollination triggered")
 	particles.restart()
+	unbloomed_mesh.visible = false;
+	bloomed_mesh.visible = true;
 	if particles.draw_pass_1:
 		particles.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
 	pollinated_count += 1
